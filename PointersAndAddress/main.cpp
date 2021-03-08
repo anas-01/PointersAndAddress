8aaa0178583083452c1429b8cf7ce9405d3dd892
anas-01 <71329612+anas-01@users.noreply.github.com>
2021-03-08 14:38:17 +0000

    slight changes

--- PointersAndAddress/main.cpp
+++ PointersAndAddress/main.cpp
@@ -5,42 +5,41 @@ using namespace std;
 
 int main()
 {
-  
-    int givenInt = 32;
-    float givenFloat = 64.212;
-    double givenDouble = 4.76545;
-    string givenString = "Hey look at me! I know pointers!";
-    char givenChar = '*';
 
-    int *pointerToInt = &givenInt;
-    float *pointerToFloat = &givenFloat;
-    double *pointerToDouble = &givenDouble;
-    string *pointerToString = &givenString;
-    char *pointerToChar = &givenChar;
+	int givenInt = 32;
+	float givenFloat = 64.212;
+	double givenDouble = 4.76545;
+	string givenString = "Hey look at me! I know pointers!";
+	char givenChar = '*';
 
-    cout << "Pointers and Address Location";
-    cout << "\n";
-    cout << "\n";
- 
-    cout << "Value of givenInt: " << *pointerToInt << '\n';
-    cout << "Address location of givenInt: " << pointerToInt << '\n';
-    cout << "\n";
-   
-    cout << "Value of givenFloat: " << *pointerToFloat << '\n';
-    cout << "Address location of givenFloat: " << pointerToFloat << '\n';
-    cout << "\n";
+	int* pointerToInt = &givenInt;
+	float* pointerToFloat = &givenFloat;
+	double* pointerToDouble = &givenDouble;
+	string* pointerToString = &givenString;
+	char* pointerToChar = &givenChar;
 
-    cout << "Value of givenDouble: " << *pointerToDouble << '\n';
-    cout << "Address location of givenDouble: " << pointerToDouble << '\n';
-    cout << "\n";
+	cout << "Pointers and Address Memory Location" << '\n';
+	cout << "\n";
 
-    cout << "Value of givenString: " << *pointerToString << '\n';
-    cout << "Address location of givenString: " << pointerToString << '\n';
-    cout << "\n";
+	cout << "Value of givenInt: " << *pointerToInt << '\n';
+	cout << "Address location of givenInt: " << pointerToInt << '\n';
+	cout << "\n";
 
-    cout << "Value of givenChar: " << *pointerToChar << '\n';
-    cout << "Address location of givenChar: " << pointerToChar << '\n';
-    cout << "\n";
+	cout << "Value of givenFloat: " << *pointerToFloat << '\n';
+	cout << "Address location of givenFloat: " << pointerToFloat << '\n';
+	cout << "\n";
 
-    return 0;
+	cout << "Value of givenDouble: " << *pointerToDouble << '\n';
+	cout << "Address location of givenDouble: " << pointerToDouble << '\n';
+	cout << "\n";
+
+	cout << "Value of givenString: " << *pointerToString << '\n';
+	cout << "Address location of givenString: " << pointerToString << '\n';
+	cout << "\n";
+
+	cout << "Value of givenChar: " << *pointerToChar << '\n';
+	cout << "Address location of givenChar: " << pointerToChar << '\n';
+	cout << "\n";
+
+	return 0;
 }
